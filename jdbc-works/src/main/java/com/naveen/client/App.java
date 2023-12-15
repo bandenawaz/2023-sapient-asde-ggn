e7eb07f5bd08c31f2eb6fd525c6a96947b7bcbf0
Naveen <adith.naveen@gmail.com>
2023-12-15 12:38:09 +0530

    updated with jdbc work

--- jdbc-works/src/main/java/com/naveen/client/App.java
+++ jdbc-works/src/main/java/com/naveen/client/App.java
@@ -4,6 +4,8 @@ import com.naveen.beans.Employee;
 import com.naveen.contracts.IEmployeeDAO;
 import com.naveen.dao.EmployeeDAO;
 
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
@@ -14,7 +16,17 @@ public class App {
 
 //        System.out.println(dao.insertEmployee(employee)?"Inserted" :"Not Inserted");
 
-        dao.deleteEmployee(101);
+//        dao.deleteEmployee(101);
+
+//        final Employee employee1 = dao.getEmployee(103);
+//        System.out.println(employee1);
+
+        final List<Employee> employees = dao.getEmployees();
+
+        for(Employee emp : employees) {
+            System.out.println(emp);
+        }
+
 
     }
 
--- jdbc-works/src/main/java/com/naveen/dao/EmployeeDAO.java
+++ jdbc-works/src/main/java/com/naveen/dao/EmployeeDAO.java
@@ -5,7 +5,9 @@ import com.naveen.contracts.IEmployeeDAO;
 import com.naveen.util.GetConnection;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDAO implements IEmployeeDAO {
@@ -33,12 +35,50 @@ public class EmployeeDAO implements IEmployeeDAO {
 
     @Override
     public Employee getEmployee(int empId) {
+        String sql  = "select empid, empname, empsalary, empemail from employee where empid = ?";
+
+        try {
+            PreparedStatement ps = GetConnection.getMysqlConnection().prepareStatement(sql);
+            ps.setInt(1, empId);
+
+            final ResultSet resultSet = ps.executeQuery();
+
+            if(resultSet.next()) {
+                Employee temp = new Employee();
+                temp.setEmpId(empId);
+                temp.setEmpName(resultSet.getString(2));
+                temp.setEmpSalary(resultSet.getDouble(3));
+                temp.setEmpEmail(resultSet.getString(4));
+                return temp;
+            }
+        }catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public List<Employee> getEmployees() {
-        return null;
+        String sql  = "select empid, empname, empsalary, empemail from employee";
+        List<Employee> list = null ;
+        try {
+            PreparedStatement ps = GetConnection.getMysqlConnection().prepareStatement(sql);
+            list = new ArrayList<>();
+            final ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                Employee temp = new Employee();
+                temp.setEmpId(resultSet.getInt(1));
+                temp.setEmpName(resultSet.getString(2));
+                temp.setEmpSalary(resultSet.getDouble(3));
+                temp.setEmpEmail(resultSet.getString(4));
+
+                list.add(temp);
+            }
+        }catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return  list;
+
     }
 
     @Override
@@ -61,7 +101,7 @@ public class EmployeeDAO implements IEmployeeDAO {
 
     @Override
     public Employee updateEmployeeEmail(int empId, String email) {
-        // TODO 
+        // TODO
         return null;
     }
 }
